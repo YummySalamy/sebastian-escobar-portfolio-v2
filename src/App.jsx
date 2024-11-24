817d0195dff6d44bb73c0c794c3c41e964e5f38d
Sebastian <carsebastian1013@gmail.com>
2024-11-23 22:38:15 -0500

    changed the landing page route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 // General Views imports
-import LandingPage from './views/Landingpage'
+import LandingPage from './views/LandingPage'
 import MyProjects from './views/Projects'
 import NotFound from './views/404_NotFound'
 
